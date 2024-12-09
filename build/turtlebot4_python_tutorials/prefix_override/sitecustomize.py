f568e6a98bd5489d89f83b06412dfd6498c99c57
sartaz2 <msaifsartaz2@gmail.com>
2024-12-09 14:43:56 -0500

    Add files via upload

---
+++ build/turtlebot4_python_tutorials/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/turtlebot/turtlebot4_ws/install/turtlebot4_python_tutorials'
---
+++ build/turtlebot4_python_tutorials/share/turtlebot4_python_tutorials/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/turtlebot/turtlebot4_ws/build/turtlebot4_python_tutorials"
